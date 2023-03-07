038dab654d22c8148d84ac794c5cf6c9e3868d58
gary909 <garywhite61@hotmail.com>
2023-03-07 12:33:12 +0100

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function sumMul(n,m){
+    //your idea here
+}
+
+console.log(sumMul(0,0)); // "INVALID"
+console.log(sumMul(2,9)); // 20
+console.log(sumMul(4,-7)); // "INVALID"
+console.log(sumMul(3,13)); // 30
+console.log(sumMul(4,123)); // 1860
